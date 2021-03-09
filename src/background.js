ee21051c03234944e707d37babb6150d7569628b
williamD <william.donval@gmail.com>
2021-03-09 21:38:40 +0100

    fix

--- src/background.js
+++ src/background.js
@@ -31,7 +31,7 @@ async function createWindow() {
 	if (process.env.WEBPACK_DEV_SERVER_URL) {
 		// Load the url of the dev server if in development mode
 		await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
-		if (!process.env.IS_TEST) win.webContents.openDevTools();
+		//if (!process.env.IS_TEST) win.webContents.openDevTools();
 	} else {
 		createProtocol("app");
 		// Load the index.html when not in development
@@ -58,14 +58,14 @@ app.on("activate", () => {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on("ready", async () => {
-	if (isDevelopment && !process.env.IS_TEST) {
+	/*if (isDevelopment && !process.env.IS_TEST) {
 		// Install Vue Devtools
 		try {
 			await installExtension(VUEJS_DEVTOOLS);
 		} catch (e) {
 			console.error("Vue Devtools failed to install:", e.toString());
 		}
-	}
+	}*/
 	createWindow();
 });
 
