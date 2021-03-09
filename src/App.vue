d01ec3773215282c644b4ebad2d10202745aacc2
williamD <william.donval@gmail.com>
2021-03-09 21:39:06 +0100

    set settings file values to vuex store

--- src/App.vue
+++ src/App.vue
@@ -5,5 +5,28 @@
 </template>
 
 <script>
-	export default {};
+	const Store = require("electron-store");
+	const electronStore = new Store();
+
+	export default {
+		created() {
+			this.initialize();
+		},
+		methods: {
+			initialize() {
+				const language = electronStore.get("settings.general.language");
+				const theme = electronStore.get("settings.general.theme");
+				const outputPath = electronStore.get("settings.general.outputPath");
+				if (language) {
+					this.$store.dispatch("setLanguage", language);
+				}
+				if (theme) {
+					this.$store.dispatch("setTheme", theme);
+				}
+				if (outputPath) {
+					this.$store.dispatch("setOutputPath", outputPath);
+				}
+			},
+		},
+	};
 </script>
